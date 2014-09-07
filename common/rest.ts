d10fe63967fc79513e90a67363632c99fa917b7f
Mehuge <adf@sorcerer.co.uk>
2014-09-07 23:11:00 +0100

    Updated rest API to include support for more API calls.  Work in progress.

--- common/rest.ts
+++ common/rest.ts
@@ -1,5 +1,8 @@
 /// <reference path="../vendor/jquery.d.ts" />
 module Rest {
+
+    var servers = [], server: string = "Hatchery";
+
     // Basic promise API
     export function Promise(fn) {
         var then,
@@ -18,9 +21,9 @@ module Rest {
             };
         setTimeout(function () { context = fn(fulfill, reject) || this; }, 0);
         return {
-            then: function (fn, fnr) {
-                then = fn;
-                rejected = fnr;
+            then: function (success, fail?) {
+                then = success;
+                rejected = fail;
             },
             resolve: function () {
                 fulfill.apply(self, arguments);
@@ -31,8 +34,41 @@ module Rest {
         };
     }
 
-    export function call(verb, params) {
-        var serverURI = (typeof cuAPI == "undefined" ? {} : cuAPI).serverURL || "http://chat.camelotunchained.com:8000/api/";
+    function getServerInfo(server?: string) {
+        if (server) {
+            for (var i = 0; i < servers.length; i++) {
+                if (servers[i].name === server) {
+                    return servers[i];
+                }
+            }
+        }
+        return {
+            host: "chat.camelotunchained.com"
+        };
+    }
+
+    function getServerURI(verb: string) {
+        var host: string, port: number = 8000 , protocol : string = "http:";
+        switch (verb) {
+            case "servers":
+                port = 8001;
+                host = getServerInfo().host;
+                break;
+            case "characters":
+                protocol = "https:";
+                port = 4443;
+                host = getServerInfo(server).host;
+                break;
+            default:
+                if (typeof cuAPI !== "undefined" && "serverURL" in cuAPI) return cuAPI.serverURL;
+                host = getServerInfo(server).host;
+                break;
+        }
+        return protocol + "//" + host + ":" + port + "/api/";
+    }
+
+    export function call(verb, params?) {
+        var serverURI = getServerURI(verb);
 
         // Raw call the CU RESI API, returns a promise
         params = params || {};
@@ -44,7 +80,7 @@ module Rest {
                 async: true, cache: false,
                 accepts: params.accepts || "text/json",
                 contentType: params.contentType,
-                error: function (jqXHR: JQueryXHR, textStatus: String, errorThrown: String) {
+                error: function (jqXHR: JQueryXHR, textStatus: string, errorThrown: string) {
                     reject(textStatus, errorThrown, jqXHR);
                 }
             }).done(function (data) {
@@ -53,7 +89,55 @@ module Rest {
         });
     }
 
-    export function getKills(query) {
-        return call("kills", { type: "GET", query: query });
+    export function selectServer(name: string) {
+        server = name;
     }
+
+    export function getServers() {
+        return Promise(function (fulfill, reject) {
+            call("servers").then(function (list) {
+                servers = list;
+                fulfill(servers);
+            },reject);
+        });
+    }
+
+    export function getFactions() {
+        return call("game/factions");
+    }
+
+    export function getRaces() {
+        return call("game/races");
+    }
+
+    export function getPlayers() {
+        return call("game/players");
+    }
+
+    export function getCharacters(loginToken: string) {
+        return call("characters", { query: { loginToken: loginToken } });
+    }
+
+    export function getAbilities() {
+        return call("abilities");
+    }
+
+    export function getPatchNotes() {
+        return call("patchnotes");
+    }
+
+    export function getBanners() {
+        return call("banners");
+    }
+
+    export function getEvents() {
+        return call("scheduledevents");
+    }
+
+    export function getKills(query) {
+        return call("kills", { query: query });
+    }
+
+    // pre-load server list
+    getServers();
 }
