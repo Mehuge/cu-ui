01e49d8865319a94a86fcac511ecee083e9b2ed3
Mehuge <adf@sorcerer.co.uk>
2014-09-24 21:12:35 +0100

    Merge mehuge-ui & cse-master back into mehuge-ui

---
+++ addons/addons.ts
@@ -0,0 +1,90 @@
+﻿/// <reference path="../vendor/jquery.d.ts" />
+/// <reference path="../module-list.d.ts" />
+module Addons {
+
+    var ml: JQuery = $('#moduleList');
+    var modules = UI.Modules;
+
+    // Loads the named UI module.
+    function OpenUI(name) {
+        console.log('addons: loading ' + name + '.ui');
+        try {
+            if (name != "addons" && cuAPI != undefined) cuAPI.OpenUI(name + ".ui");
+            return modules[name].loaded = true;
+        } catch (e) {
+            modules[name].loaded = false;
+        }
+    }
+
+    // Loads the named UI module.
+    function CloseUI(name) {
+        console.log('addons: closing ' + name + '.ui');
+        try {
+            if (cuAPI != undefined) {
+                cuAPI.CloseUI(name);
+            }
+            modules[name].loaded = false;
+			return true;
+        } catch (e) {
+			console.warn('failed to close UI ' + JSON.stringify(e));
+        }
+    }
+
+    // Initialise the UI (module list) and auto-load any modules marked as autoLoad true
+    function _init() {
+        if (ml) {
+            var html = '<table><tr><th>Name</th><th>Core</th><th>Loaded</th>';
+            for (var name in modules) {
+                var addon = modules[name];
+                if (addon.autoLoad) {
+                    OpenUI(name);
+                }
+                html += '<tr id="'+name+'">'
+                + '<td>' + name + '</td>'
+                + '<td>' + (addon.core ? '&#x2714;' : '') + '</td>'
+                + '<td>' + (addon.core||addon.loaded ? '&#x2714;' : '') + '</td>'
+                + '</tr>';
+            }
+            html += '</html>';
+            ml.append(html);
+
+            // close the UI
+            document.getElementById("close").onclick = () => {
+                Hide();
+            };
+
+            // Attach event listeners to the buttons.
+            ml.find('tr').each((index: number, elem: Element) => {
+                elem.addEventListener("click", (ev: PointerEvent) => {
+                    var name = elem.getAttribute("id"),
+                        el: JQuery = $(elem).find('td:last-child'),
+                        text = el.text();
+                    if (name && text === "") {
+                        if (OpenUI(name)) {
+							el.html('&#x2714;');
+						}
+                    } else {
+                        if (CloseUI(name)) {
+							el.html('');
+						}
+                    }
+                });
+            });
+        }
+        // Show();
+        CloseUI("addons");
+    };
+ 
+    // Show the addons UI
+    function Show() {
+        document.body.style.display = 'block';
+    };
+
+    // Hide the addons UI
+    function Hide() {
+        document.body.style.display = 'none';
+    }
+ 
+    // Initialise
+    _init();
+};
---
+++ module-list.js
@@ -0,0 +1,19 @@
+﻿var UI = UI || {};
+UI.Modules = {
+    "character": { core: true },
+    "chat": { core: true },
+    "errormessages": { core: true },
+    "kills": { core: true },
+    "login": { core: true },
+    "perfhud": { core: true },
+    "respawn": { core: true },
+    "skillbar": { core: true },
+    "target": { core: true },
+    "addons": { core: false, autoLoad: true },
+    // custom-ui's go below...
+    // "example": { core: false, autoLoad: true }
+    "lb": { core: false, autoLoad: true },
+    "heatmap": { core: false, autoLoad: true },
+    "pledges": { core: false }
+
+};
