bba98617ec3b3bc6e9ccfbf7084c9d2f8daa1542
Mehuge <adf@sorcerer.co.uk>
2014-10-08 23:21:00 +0100

    Generate tracker graph data from DB

---
+++ web/data-daily.php
@@ -0,0 +1,32 @@
+<?php
+$load = sys_getloadavg();
+if ($load[0] > 80) {
+    header('HTTP/1.1 503 Too busy, try again later');
+    die('var tracker = { error: "503 Too Busy, try again later" };');
+}
+$db = mysql_connect("localhost", "cu", "tracker");
+if ($db) {
+	echo 'var _cu_tracker={';
+    mysql_select_db("cu",$db) || die("failed to select database");
+	$q = mysql_query("select goal, name, timestamp FROM cu.stretch ORDER by timestamp DESC");
+    if ($q) {
+		echo 'goals: [';
+		$comma = '';
+		while ($r = mysql_fetch_row($q)) {
+			$goal = $r[0];
+			$name = $r[1];
+			echo $comma.'{ goal:'.$r[0].',name:"'.$r[1].'"}';
+			$comma = ',';
+		}
+		echo '],';
+	}
+	$q = mysql_query("select convert(unix_timestamp(date(timestamp))/60/60/24,UNSIGNED) AS DAY, max(current), sum(delta), sum(time_delta) AS DAY FROM cu.tracker GROUP BY DAY ORDER BY DAY");
+    if ($q) {
+		echo 'data:[';
+        while ($r = mysql_fetch_row($q)) {
+			echo '["'.$r[0].'",'.$r[1].','.$r[2].','.$r[3].'],';
+		}
+		echo ']};';
+	}
+}
+?>
