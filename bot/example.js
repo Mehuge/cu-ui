25f5c17c9e3aac779813dc15ed334092ac0f44da
Mehuge <adf@sorcerer.co.uk>
2014-10-11 00:50:34 +0100

    Update bot to check if hit a wall (stopped moving).  Added first stab at a heal bot.

--- bot/example.js
+++ bot/example.js
@@ -1,8 +1,13 @@
-var targets = {}, searching = 0;
+var targets = {}, searching = 0, wanderer = 0;
 function rand(n) {
     return Math.random() * n;
 }
 function run() {
+    function walk(direction, duration) {
+        for (var i = 0; i < duration; i++) {
+            BOT.walk(direction, 1);
+        }
+    }
     BOT.health()
         .target()
         .start({
@@ -12,11 +17,20 @@ function run() {
             health: function (characterName, hp, maxHP, selfEffects) {
                 if (hp === 0) {
                     console.log("RESURECT!!!!");
-                    BOT.reset().sleep(1).key(KR).sleep(2).walk(FORWARDS, 30);
+                    BOT.reset().sleep(1).key(KR).sleep(2);
+                    walk(FORWARDS, 30);
                 } else if (hp < 25) {
                     BOT.reset().attack(Am, 20);
                 } else {
                     console.log('HP GOOD');
+                }
+            },
+            moving: function() {
+                console.log('I AM WALKING = SPEED = ' + cuAPI.speed);
+                if (cuAPI.speed < 1.0) {
+                    BOT.reset().cancel().health().turn(200);
+                    walk(FORWARDS, 5);
+                    BOT.turn(-200);
                 }
             },
             target: function (friendly, name, hp, max, effects) {
@@ -27,10 +41,12 @@ function run() {
                 var wander = function () {
                     var turnAmount = -400 + rand(800);
                     console.log("BOT: WANDER turn amount = " + turnAmount);
-                    BOT.reset().click().walk(BACKWARDS, 2).turn(turnAmount).walk(FORWARDS, 5);
+                    BOT.reset().click().turn(wanderer % 2 == 0 ? -300 : 300);
+                    wanderer++;
+                    walk(FORWARDS, 5 + rand(10));
                     nextTarget();
                 };
-                var locate = function () {
+                var spinlocate = function () {
                     searching++;
                     if (searching == 13) {
                         console.log('searching too long, wander about!');
@@ -55,7 +71,7 @@ function run() {
                 }
                 if (hp == -1) {
                     console.log('No Target, try and find a target ... ');
-                    locate();
+                    spinlocate();
                 } else {
                     var target = targets[name] = targets[name] || { ohp: 0, lastAttacked: 0, fails: 0 }, now = Date.now();
                     console.log('have target: oldHP ' + target.ohp + ' hp ' + hp);
@@ -75,7 +91,9 @@ function run() {
                                 target.fails = 0;
                             } else {
                                 console.log('Target not loosing HPs.  Get closer?');
-                                BOT.reset().sleep(0.5).walk(FORWARDS, 2).click();
+                                BOT.reset().sleep(0.5);
+                                walk(FORWARDS, 2);
+                                BOT.click();
                                 nextTarget();
                                 target.fails++;
                                 target.lastAttacked = 0;
