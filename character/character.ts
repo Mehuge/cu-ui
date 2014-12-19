cb921e1f1ff0db91a4741c733ccf9745724b184d
Mehuge <adf@sorcerer.co.uk>
2014-12-19 23:13:34 +0000

    Re-instate auto-loading of custom addons

--- character/character.ts
+++ character/character.ts
@@ -69,6 +69,9 @@ module Character {
             cuAPI.OnCharacterStaminaChanged(updateStamina);
 
             cuAPI.OnCharacterEffectsChanged(updateEffects);
+
+            // Launch addons UI
+            cuAPI.OpenUI("addons.ui");
         });
     }
 }
