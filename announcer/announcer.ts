13b4f2b7fa762bc74275a2164a9abb696213ddb2
Mehuge <adf@sorcerer.co.uk>
2015-01-24 01:10:28 +0000

    Prototype announcer UI (UT style).  Doesn't work yet due to API limitations.

---
+++ announcer/announcer.ts
@@ -0,0 +1,69 @@
+﻿module Announcer {
+
+    var player: string;
+    var selfKills = [];
+
+    function Announce(who) {
+        if (who[1] === player) {
+            selfKills = [];
+            (new Audio("WilhelmScream.ogg")).play();
+        } else if (who[0] === player) {
+            selfKills.push(Date.now());
+            if (selfKills.length > 1) {
+                var dur = selfKills[selfKills.length - 1] - selfKills[0];
+                console.log('kill frequency ' + (dur / selfKills.length));
+                if (dur / selfKills.length < 1000) {
+                    switch (selfKills.length) {
+                        case 2: (new Audio("doublekill.ogg")).play(); break;
+                        case 3: (new Audio("multikill.ogg")).play(); break;
+                        case 4: (new Audio("megakill.ogg")).play(); break;
+                        case 5: (new Audio("ultrakill.ogg")).play(); break;
+                        default: (new Audio("monsterkill.ogg")).play(); break;
+                    }
+                    return;
+                }
+                if (selfKills.length > 2) {
+                    switch (selfKills.length) {
+                        case 3: (new Audio("killingspree.ogg")).play(); break;
+                        case 4: (new Audio("rampage.ogg")).play(); break;
+                        case 5: (new Audio("dominating.ogg")).play(); break;
+                        case 6: (new Audio("unstoppable.ogg")).play(); break;
+                        default: (new Audio("godlike.ogg")).play(); break;
+                    }
+                }
+            }
+        }
+    }
+
+    function OnChat(type, from, body, nick, iscse) {
+        switch (type) {
+        case XmppMessageType.GROUPCHAT:
+            var user = from.split("@")[0];
+            if (user === "_combat") {
+                Announce(body.substr(0,body.length-1).split(" killed "));
+            }
+            break;
+        }
+    }
+
+    function init() {
+        cuAPI.OnCharacterNameChanged((name: string) => {
+            player = name;
+        });
+        cuAPI.OnChat(OnChat);
+        setTimeout(function () {
+            cuAPI.OpenUI("chat.ui");
+        }, 5000);
+    }
+
+    // initialise 
+    if (typeof cuAPI !== "undefined") {
+        cuAPI.CloseUI("chat");
+        if (cuAPI.initialized) {  // already initialised
+            init();
+        } else {
+            cuAPI.OnInitialized(init);
+        }
+    } 
+
+}; 
