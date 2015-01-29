5c9c3fc1a5328d2dca3b1cf9aa1d35fb74a19b31
Mehuge <adf@sorcerer.co.uk>
2015-01-29 22:53:02 +0000

    Killing 45 people at once caused the announcer to bombard you with a mash of audio that was painful to listen to.  Introduce a sound queue, and only play the last triggered announcement discarding other queued items.

--- announcer/announcer.ts
+++ announcer/announcer.ts
@@ -2,11 +2,37 @@
 
     var player: string;
     var selfKills = [];
+    var announcements = [], playing;
+
+    var play = function (name) {
+        // playing a new sound overrides all but the currently playing sound
+        announcements.push({ time: Date.now(), name: name });
+        if (!playing) {
+            playing = true;
+            setTimeout(playAll, 50);
+        }
+    };
+
+    var playAll = function () {
+        // we only play the last sound queue, and then discard the rest
+        playing = announcements.pop();
+        while (announcements.shift());      // drain announcement queue
+        console.log(playing.name);
+        var audio = new Audio(playing.name);
+        audio.onended = function () {
+            if (announcements.length) {
+                setTimeout(playAll, 50);
+            } else {
+                playing = null;
+            }
+        };
+        audio.play();
+    };
 
     function Announce(who) {
         if (who[1] === player) {
             selfKills = [];
-            (new Audio("WilhelmScream.ogg")).play();
+            play("WilhelmScream.ogg");
         } else if (who[0] === player) {
             selfKills.push(Date.now());
             if (selfKills.length > 1) {
@@ -18,24 +44,23 @@
                     }
                     count++;
                 }
-                console.log(JSON.stringify(selfKills));
                 if (count > 1) {
                     switch (selfKills.length) {
-                        case 2: (new Audio("doublekill.ogg")).play(); break;
-                        case 3: (new Audio("multikill.ogg")).play(); break;
-                        case 4: (new Audio("megakill.ogg")).play(); break;
-                        case 5: (new Audio("ultrakill.ogg")).play(); break;
-                        default: (new Audio("monsterkill.ogg")).play(); break;
+                        case 2: play("doublekill.ogg"); break;
+                        case 3: play("multikill.ogg"); break;
+                        case 4: play("megakill.ogg"); break;
+                        case 5: play("ultrakill.ogg"); break;
+                        default: play("monsterkill.ogg"); break;
                     }
                     return;
                 }
                 if (selfKills.length > 2) {
                     switch (selfKills.length) {
-                        case 3: (new Audio("killingspree.ogg")).play(); break;
-                        case 4: (new Audio("rampage.ogg")).play(); break;
-                        case 5: (new Audio("dominating.ogg")).play(); break;
-                        case 6: (new Audio("unstoppable.ogg")).play(); break;
-                        default: (new Audio("godlike.ogg")).play(); break;
+                        case 3: play("killingspree.ogg"); break;
+                        case 4: play("rampage.ogg"); break;
+                        case 5: play("dominating.ogg"); break;
+                        case 6: play("unstoppable.ogg"); break;
+                        default: play("godlike.ogg"); break;
                     }
                 }
             }
