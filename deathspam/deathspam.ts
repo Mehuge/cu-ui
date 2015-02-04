26617c8cb64ab8b86d05ccca27b4bc01282ba42b
Mehuge <adf@sorcerer.co.uk>
2015-02-04 22:00:30 +0000

    Deathspam improvements - Different colour for suicides - Fading now does not scroll (looks tonnes better) - Own name replaced with You/you/yourself. - Handles long deathspam properly (no chopping off of the bottom line)

--- deathspam/deathspam.ts
+++ deathspam/deathspam.ts
@@ -4,19 +4,34 @@
 
     export function DeathSpam(text) {
         var a = text.split(" killed ");
+        a[1] = a[1].substr(0, a[1].length - 1);
         var div = document.createElement("div");
-        div.className = 'other';
-        if (a[0] === player) div.className = 'killer';
-        if (a[1].substr(0, a[1].length - 1) === player) div.className = 'killed';
+
+        if (a[1] === player && a[0] === player) {
+            div.className = 'suicide';
+            text = 'You killed yourself! lololol';
+        } else if (a[0] === player) {
+            div.className = 'killer';
+            a[0] = 'You'
+            text = a[0] + ' killed ' + a[1] + '.';
+        } else if (a[1] === player) {
+            div.className = 'killed';
+            a[1] = 'you'
+            text = a[0] + ' killed ' + a[1] + '.';
+        } else {
+            div.className = 'other';
+        }
         div.className += " shadow";
         div.textContent = text;
         deathspam.appendChild(div);
-        if (deathspam.offsetHeight > maxHeight) {
+        var h = deathspam.offsetHeight;
+        while (h > maxHeight) {
             var remove = deathspam.children[0];
             if (remove._fadeOutTimer) {
                 clearTimeout(remove._fadeOutTimer);
                 remove._fadeOutTimer = null;
             }
+            h -= remove.offsetHeight;
             deathspam.removeChild(remove);
         }
         (function (fade) {
@@ -24,11 +39,12 @@
                 if (fade._fadeOutTimer) {
                     var op = +(fade.style.opacity || 1.0);
                     fade.className = fade.className.replace(" shadow", "");
+                    // console.log('Opacity : ' + op + ' x10 > 1 ' + (((op * 10) | 0) > 1));
                     if (((op*10)|0) > 1) {                  // crude way to round to 1 decimal place
                         fade.style.opacity = op - 0.025;
                         fade._fadeOutTimer = setTimeout(fadeOut, 100);
                     } else {
-                        deathspam.removeChild(fade);
+                        fade.style.opacity = 0;
                     }
                 }
             }
