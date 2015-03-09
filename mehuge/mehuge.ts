ac851358b556aeeb3fc845391fc1e9b849c1ee10
Mehuge <adf@sorcerer.co.uk>
2015-03-09 01:49:39 +0000

    Upgrade to support joining normal channels.

--- mehuge/mehuge.ts
+++ mehuge/mehuge.ts
@@ -1,30 +1,45 @@
 ﻿/// <reference path="xmpp.ts" />
 module Mehuge {
     var listeners = [];
-    export function connect(loginToken: string, channel: any, ready: () => void) {
-        var joined: number = 0;
+
+    // return the cooked channel name (base64 encoded channles are $_ prefixed)
+    function _channelName(name: string) {
+        // possible base64 encoded channel name without a $_ prefix, add the $_ prefix 
+        // (for backward compatibility)
+        // can remove this eventually
+        if (name.substr(-1) === "=" && name.substr(0, 2) !== "$_") {
+            return "$_" + name;
+        }
+        return name;
+    }
+
+    // Connect to chat channels.
+    export function connect(loginToken: string, channel: any, ready: (channel:string) => void) {
+        var joined: number = 0, i: number;
+
+        // We support a few ways of specifying the channel.
+        if (typeof channel === "string") channel = [channel];
         Xmpp.login(loginToken);
-        Xmpp.listen(function (e) {
-            switch (e.type) {
+        Xmpp.listen(function (ev:any) {
+            switch (ev.type) {
                 case "connected":
-                    if (typeof channel === "string") channel = [channel];
                     for (var i = 0; i < channel.length; i++) {
-                        Xmpp.join("$_" + channel[i]);
+                        Xmpp.join(_channelName(channel[i]));
                     }
                     break;
                 case "joined":
-                    if (++joined === channel.length) {
-                        ready();
-                    }
+                    ready(ev);
                     break;
                 case "groupchat":
-                    try {
-                        var o = JSON.parse(e.body);
-                    } catch (e) {
-                        o = { message: e.body };
-                        break;
+                    var room = ev.from.split("@")[0], o: any;
+                    if (room.substr(0, 2) === "$_") {
+                        try { o = JSON.parse(ev.body); } catch (e) { }
                     }
-                    o.account = e.from.split("/")[1];
+                    if (!o) {
+                        o = { message: ev.body, id: ev.id };
+                    }
+                    o.from = room;
+                    o.account = ev.from.split("/")[1];
                     for (var i = 0; i < listeners.length; i++) {
                         if (listeners[i]) {
                             listeners[i](o);
@@ -37,9 +52,17 @@ module Mehuge {
     export function listen(listener: (any) => void) {
         listeners.push(listener);
     };
+    export function join(room: string = undefined) {
+        if (room) room = _channelName(room);
+        Xmpp.join(room);
+    }
     export function send(o: any, room: string = undefined) {
-        if (room) room = "$_" + room;
+        if (room) room = _channelName(room);
         Xmpp.sendMessage(JSON.stringify(o), room);
+    }
+    export function sendText(message: any, room: string = undefined) {
+        if (room) room = _channelName(room);
+        Xmpp.sendMessage(message, room);
     }
     export function disconnect() {
         Xmpp.disconnect();
