5664070735f31d9518e6fb5be3861317688c6bff
Mehuge <adf@sorcerer.co.uk>
2015-03-16 16:59:37 +0000

    Add mehuge event manager (pub/sub support) - disabled atm until API updated

---
+++ mehuge/mehuge-events.ts
@@ -0,0 +1,36 @@
+﻿module MehugeEvents {
+    var subscribers: any = {};
+    var listener;
+/*
+    export function sub(topic: string, handler: (data: any) => void) {
+        var subs = subscribers[topic] = subscribers[topic] || { listeners: [] };
+        subs.listeners.push({ listener: handler });
+
+        // first handler for any topic?  Register event handler
+        if (!listener) {
+            cuAPI.OnEvent(listener = function (topic: string, data: any) {
+                var listeners = subscribers[topic].listeners, parsedData;
+                try {
+                    parsedData = JSON.parse(data);
+                } catch (e) {
+                    parsedData = data;
+                }
+                for (var i = 0; i < listeners.length; i++) {
+                    if (listeners[i].listener) {
+                        listeners[i].listener(event, parsedData);
+                    }
+                }
+            });
+        }
+
+        // first handler for this topic, ask client to send these events
+        if (subs.handlers.length === 1) {
+            cuAPI.Listen(topic);
+        }
+    }
+
+    export function pub(topic: string, data: any) {
+        cuAPI.Fire(topic, JSON.stringify(data));
+    }
+*/
+}
