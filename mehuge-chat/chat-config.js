5c75239872a4e3b6927f20bcdd4e0e9d28ef8468
Mehuge <adf@sorcerer.co.uk>
2015-03-20 00:24:23 +0000

    Migrate UI loading from autoexec to mehuge-chat.  Only mehuge-chat now loaded by autoexec.  Done like this because we want mehuge-chat to be loaded and ready first.

--- mehuge-chat/chat-config.js
+++ mehuge-chat/chat-config.js
@@ -3,6 +3,20 @@ ChatConfig = {
     leave: [],
     hideDeathSpam: false,
     autoexec: [
-        "jumpdelay 0"
-    ]
+        "/closeui perfhud",
+        "jumpdelay 0",
+        "/sleep 1000",
+        "/openui mehuge-lb",
+        "/openui mehuge-heatmap",
+        "/openui mehuge-perf",
+        "/openui mehuge-pop",
+        "/openui mehuge-bct",
+        "/openui mehuge-announcer",
+        "/openui mehuge-deathspam",
+        "/openui mehuge-loc",
+        "/openui mehuge-group",
+        "/openui mehuge-combatlog",
+        "/openui castbar",
+        "/openui ortu-compass"
+]
 };
--- module-list.js
+++ module-list.js
@@ -13,25 +13,8 @@ UI.Modules = {
     "equippedgear": { core: true },
     "inventory": { core: true },
     // custom-ui's go below...
-    "mehuge-lb": { core: false, autoLoad: true },
-    "mehuge-heatmap": { core: false, autoLoad: true },
-    "mehuge-perf": { core: false, autoLoad: true },
-    "mehuge-pop": { core: false, autoLoad: true },
-    "mehuge-bct": { core: false, autoLoad: true },
-    "mehuge-announcer": { core: false, autoLoad: true },
-    "mehuge-deathspam": { core: false, autoLoad: true },
-    "mehuge-loc": { core: false, autoLoad: true },
-    "mehuge-group": { core: false, autoLoad: true },
-    "mehuge-combatlog": { core: false, autoLoad: true },
-    "mehuge-pledges": { core: false },
-    "castbar": { core: false, autoLoad: true },
-    "ortu-compass": { core: false, autoLoad: true },
     "mehuge-chat": { core: false, autoLoad: true },
 };
-UI.Commands = [
-    "vsync 1",
-    "daytime 0"
-];
 
 // Autoexec (in offline mode)
 UI.offline = {
@@ -39,8 +22,7 @@ UI.offline = {
         "perfhud": { core: true, close: true },
         "minimap": { core: true, close: true },
         "mehuge-heatmap": { core: false, autoLoad: true },
-        "mehuge-loc": { core: false, autoLoad: true }
-},
+    },
     Commands: [
         "daytime 10",
         "fly 1"
