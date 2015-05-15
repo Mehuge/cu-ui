4605ac1ce7ce5319b6dba7d0b07bb2f609070682
Mehuge <adf@sorcerer.co.uk>
2015-05-15 23:24:08 +0100

    mehuge-lights UI - allows dropping of lights based on current set parameters with click of the drop button

---
+++ mehuge-lights/lights.ts
@@ -0,0 +1,52 @@
+﻿module Lights {
+    var settings: any = {
+        intensity: 2,
+        radius: 50,
+        red: 255,
+        green: 255,
+        blue: 255
+    };
+
+    function update() {
+        color.style.backgroundColor = 'rgb(' + settings.red + ',' + settings.green + ',' + settings.blue + ')';
+    }
+    function changed(e) {
+        settings[e.target.id] = e.target.value | 0;
+        update();
+    }
+    function focus(e) {
+        setTimeout(function () {
+            cuAPI.RequestInputOwnership();
+        }, 10);
+    }
+    function blur(e) {
+        cuAPI.ReleaseInputOwnership();
+    }
+    function E(n) : any {
+        return document.getElementById(n);
+    }
+
+    var drop = E("drop"), color = E("color"), reset = E("reset");
+
+    for (var n in settings) {
+        var el : HTMLInputElement = E(n);
+        el.value = settings[n];
+        el.onchange = changed;
+        el.onfocus = focus;
+        el.onblur = blur;
+    }
+    update();
+
+    if (typeof cuAPI !== "undefined") {
+        cuAPI.OnInitialized(() => {
+            drop.onclick = function () {
+                cuAPI.DropLight(settings.intensity | 0, settings.radius | 0, settings.red | 0, settings.green | 0, settings.blue | 0);
+               color.focus();
+            };
+            reset.onclick = function () {
+                cuAPI.ResetLights();
+                color.focus();
+            };
+        });
+    }
+} 
